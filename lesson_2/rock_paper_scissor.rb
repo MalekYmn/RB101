af242bf78cd0ecdbebd4b37a432ed5122d5d395d
Malek Ymn <malekymn@hotmail.com>
2020-11-14 14:55:11 +0000

    rock paper scissors spock and lizard

--- lesson_2/rock_paper_scissor.rb
+++ lesson_2/rock_paper_scissor.rb
@@ -1,43 +1,71 @@
-VALID_CHOICES = ["rock", "paper", "scissors"]
+VALID_CHOICES = ["rock", "paper", "scissors", "spock", "lizard"]
+VALID_KEYS = { r: "rock", p: "paper", sc: "scissors", sp: "spock", l: "lizard" }
+WINING_MOVES = {
+  rock: ["scissors", "lizard"],
+  scissors: ["paper", "lizard"],
+  paper: ["rock", "spock"],
+  lizard: ["spock", "paper"],
+  spock: ["rock", "scissors"]
+}
 
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
 def win?(first, second)
-  (first == 'rock' && second == "scissors") ||
-    (first == 'scissors' && second == "paper") ||
-    (first == 'paper' && second == "rock")
+  WINING_MOVES[first.to_sym].include?(second)
 end
 
-def display_result(player, computer)
+def display_result(player, computer, player_wins, computer_wins)
   if win?(player, computer)
-    prompt("You won!")
+    prompt("You won #{player_wins} times!")
   elsif player == computer
     prompt("It's a tie!")
   else
-    prompt("Computer won!")
+    prompt("Computer won #{computer_wins} times!")
   end
 end
 
 loop do
-  choice = ''
-  loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    choice = Kernel.gets().chomp()
+  player_wins = 0
+  computer_wins = 0
 
-    if VALID_CHOICES.include?(choice)
+  loop do
+    choice = ''
+    loop do
+      prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+      choice = Kernel.gets().chomp()
+
+      if VALID_CHOICES.include?(choice)
+        break
+      elsif VALID_KEYS.include?(choice.to_sym)
+        choice = VALID_KEYS[choice.to_sym]
+        break
+      else
+        prompt("That's not a valid choice.")
+      end
+    end
+
+    computer_choice = VALID_CHOICES.sample
+
+    prompt("You chose #{choice}. Computer chose #{computer_choice}")
+
+    if win?(choice, computer_choice)
+      player_wins += 1
+    elsif win?(computer_choice, choice)
+      computer_wins += 1
+    end
+
+    display_result(choice, computer_choice, player_wins, computer_wins)
+
+    if player_wins == 5
+      prompt("You're the grand winner!")
       break
-    else
-      prompt("That's not a valid choice.")
+    elsif computer_wins == 5
+      prompt("computer is the grand winnner!")
+      break
     end
   end
-
-  computer_choice = VALID_CHOICES.sample
-
-  prompt("You chose #{choice}. Computer chose #{computer_choice}")
-
-  display_result(choice, computer_choice)
 
   prompt('do you want to play again?')
   answer = Kernel.gets().chomp()
